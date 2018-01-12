d98a71cfe6b2150f5ec3d718713de9db13ec86f0
byron.roberts <byron.roberts@autotrader.co.uk>
2018-01-12 13:35:24 +0000

    Initial commit

--- app/src/main/java/roberts/byron/kotlinproject/ToDoFragment.kt
+++ app/src/main/java/roberts/byron/kotlinproject/ToDoFragment.kt
@@ -1,15 +1,22 @@
 package roberts.byron.kotlinproject
 
+import android.app.DatePickerDialog
 import android.app.Fragment
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.DatePicker
 import kotlinx.android.synthetic.main.fragment_to_do.*
+import java.util.*
 
 class ToDoFragment : Fragment() {
 
-    private val toList = arrayListOf<String>()
+    private lateinit var toDoListAdapter: ToDoListRecyclerViewAdapter
+    private var toDoListItems: ArrayList<ToDoListComponent> = ArrayList()
+    private lateinit var linearLayoutManager: LinearLayoutManager
+    private var calendar: Calendar = Calendar.getInstance()
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater?.inflate(R.layout.fragment_to_do, container, false)
@@ -17,24 +24,25 @@ class ToDoFragment : Fragment() {
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        doneButton.setOnClickListener{ addElementToList() }
+        doneButton.setOnClickListener { addElementToList() }
+
+        linearLayoutManager = LinearLayoutManager(activity)
+        linearLayoutManager.isAutoMeasureEnabled = false
+        toDoListAdapter = ToDoListRecyclerViewAdapter(toDoListItems, activity)
+        toDoRecyclerView.layoutManager = linearLayoutManager
+        toDoRecyclerView.adapter = toDoListAdapter
+
+        completionDate.setOnClickListener { openDatePicker() }
+    }
+
+    private fun openDatePicker() {
+        DatePickerDialog(activity, date, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show()
     }
 
     private fun addElementToList() {
-        toList.add(toDoEntry.text.toString())
-        entered_text.text = ""
-        val stringBuilder = StringBuilder()
-        val textToShow : String?
-
-        for (element in toList) {
-            stringBuilder.append(element)
-            stringBuilder.append("\n")
-        }
-
-        textToShow = stringBuilder.toString()
-
-        entered_text.text = textToShow
+        val newElement = ToDoListComponent(toDoEntry.text.toString(), completionDate.text.toString())
+        toDoListAdapter.addListItem(newElement)
     }
 
-
+    private val date = DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth -> completionDate.setText(dayOfMonth.toString() + "/" + month.plus(1).toString() + "/" + year.toString() ) }
 }
--- app/src/main/java/roberts/byron/kotlinproject/ToDoListRecyclerViewAdapter.kt
+++ app/src/main/java/roberts/byron/kotlinproject/ToDoListRecyclerViewAdapter.kt
@@ -1,23 +1,39 @@
 package roberts.byron.kotlinproject
 
+import android.content.Context
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.to_do_list_item.view.*
 
 
-class ToDoListRecyclerViewAdapter(val toDoValues : List<ToDoListComponent>) : RecyclerView.Adapter<ToDoListRecyclerViewAdapter.ToDoViewHolder>() {
+class ToDoListRecyclerViewAdapter(private val toDoValues: ArrayList<ToDoListComponent>, private val context: Context?) : RecyclerView.Adapter<ToDoListRecyclerViewAdapter.ToDoViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ToDoViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val inflater = LayoutInflater.from(context)
+        val view = inflater.inflate(R.layout.to_do_list_item, parent, false)
+        return ToDoViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ToDoViewHolder?, position: Int) {
-
+        val toDoItem = toDoValues[position]
+        holder?.bind(toDoItem)
     }
 
     override fun getItemCount(): Int {
         return toDoValues.size
     }
 
-    class ToDoViewHolder (val view : View) : RecyclerView.ViewHolder(view)
+    fun addListItem(toDoListComponent: ToDoListComponent) {
+        toDoValues.add(toDoListComponent)
+        notifyDataSetChanged()
+    }
+
+    class ToDoViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        fun bind(toDoListComponent: ToDoListComponent) {
+            view.toDoTitle.text = toDoListComponent.title
+            view.toDoDateBy.text = toDoListComponent.dateToBeCompleted
+        }
+    }
 }
